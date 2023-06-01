c4ae0ac3cbe497e95b1d7da5e2b48a21742dc8c1
misaki04 <misakichi0043@gmail.com>
2023-06-01 18:09:28 +0900

    💄 スタイルファイルの分割

--- components/Headline.js
+++ components/Headline.js
@@ -1,16 +1,16 @@
 // import React from "react";
 // import Head from "next/head";
 // import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import classes from "./Headline.module.css";
 // import { Footer } from "../components/Footer";
 // import Links from "../components/Link";
 
 export function Headlien(props) {
   return (
     <div>
-      <h1 className={styles.title}>{props.page} Page</h1>
+      <h1 className={classes.title}>{props.page} Page</h1>
 
-      <p className={styles.description}>
+      <p className={classes.description}>
         Get started by editing {props.children}
       </p>
     </div>
--- components/Main.js
+++ components/Main.js
@@ -1,15 +1,15 @@
 // import Head from "next/head";
 // import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import classes from "./Main.module.css";
 // import { Footer } from "../components/Footer";
 import { Links } from "../components/Link";
 import { Headlien } from "../components/Headline";
 
 export function Main(props) {
   return (
-    <main className={styles.main}>
+    <main className={classes.main}>
       <Headlien page={props.page}>
-        {<code className={styles.code}>pages/{props.page}.js</code>}
+        <code className={classes.code}>pages/{props.page}.js</code>
       </Headlien>
       <Links />
     </main>
