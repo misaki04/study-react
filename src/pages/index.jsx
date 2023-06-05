ad79a5c8f1cdd403731fefa261f41bb8af00f533
misaki04 <misakichi0043@gmail.com>
2023-06-05 21:49:48 +0900

    🎨 Custom Hooksを使ってうまくまとめました

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,47 +3,14 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "src/hooks/useCounter";
+import { useInputArray } from "src/hooks/useInputArray";
+import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState("");
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow);
-  }, []);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert("５文字以内にしてください");
-    }
-    setText(e.target.value.trim());
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が存在します。");
-        return prevArray;
-      }
-      return [...prevArray, text];
-    });
-  }, [text]);
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    };
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
+  useBgLightBlue();
 
   return (
     <div className={styles.container}>
@@ -54,20 +21,16 @@ export default function Home() {
       <Header />
       {isShow ? <h1>{count}</h1> : null}
       <button onClick={handleClick}>ボタン</button>
-      <button
-        onClick={() => {
-          setIsShow((isShow) => !isShow);
-        }}
-      >
-        {isShow ? "非表示" : "表示"}
-      </button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
-      <ul>
+      <ol>
         {array.map((item) => {
           return <li key={item}>{item}</li>;
         })}
-      </ul>
+      </ol>
+
       <Main page="index" />
       <Footer />
     </div>
